fdf3ab933b5f1094661cf33c9b283163cb582337
rdotchin <richard.dotchin@gmail.com>
2016-11-01 20:25:11 -0400

    most of app.js working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,121 +4,110 @@ $(document).ready(function() {
   var round = 0;
   var correct = 0;
   var incorrect = 0;
-  var questions = 4;
-  var percent = correct / questions;
+  var percent = 0
   var time = 5;
-  var counter = setInterval(timer, 1000)
+  /*var counter = setInterval(timer, 1000)*/
 
+  /*arrays containing the questions, choices, and correct answer that the variable
+  round will cycle through when called in functions*/
+  var questions = ["number four", "number seven", "number five", "number ten"];
+  var choiceArray1 = ['four', 'seven', 'six', 'five'];
+  var choiceArray2 = ['three', 'one', 'eight', 'four'];
+  var choiceArray3 = ['seven', 'ten', 'five', 'six'];
+  var choiceArray4 = ['two', 'eight', 'four', 'ten'];
+  var correctArray = ['four', 'seven', 'five', 'ten'];
 
-  var game = {
-    question: ["question1", "question2", "question3", "question4"],
-    choice1: ["gators", "two1", "three1", "four1"],
-    choice2: ["one2", "two2", "three2", "four2"],
-    choice3: ["one3", "two3", "three3", "four3"],
-    choice4: ["one4", "two4", "three4", "four4"],
-    correct: ["gators", "two", "three", "four"]
-
-  }
-
-  var	question1 = {
-    question: "What is the mascot of University of Florida",
-    answers: ['crane', 'gator', 'bulldog', 'tide'],
-    correct: [false, true, false, false]
-    //could also use correct: 2 would say answer 3 is correct
-  };
-
-  var question2 = {
-    question: "what is the mascot of Georgia",
-    answers: ['bulldog', 'golden retriever', 'puppy', 'blah'],
-    correct: [true, false, false, false]
-  };
-
-  var question3 = {
-    question: "this is the third question",
-    answers: ['answer1', 'answer2', 'answer3', 'answer4'],
-    correct: 2
-  //this shows answer3 is correct
-  }
 
   //functions
   //--------------------------------------------------------------------
 
+  
+  //functions to set up the timer
+  //--------------------------------------------------------------------
+
+  //sets the timer interval but does not start it
+  function setTimer() {
+    counter = setInterval(timer, 1000);
+  }
+
   /*reduces time variable by 1 per second, insets the time into the #timer div, if time
-  reaches 0 stop and call the funciton for the next question*/
+  reaches 0 stop and call the funciton for the next question.  Does not start the
+  timer*/
   function timer() {
     time--;
     $('#timer').text(time + ' seconds');
-  
-  if(time == 0){
+    if(time == 0){
+      checkAnswer();
       resetTimer();
       init();
     }
   }
 
+  //resets the timer to the starting amount
   function resetTimer() {
     time = 5;
   }
 
+  //stops the timer at the end of the game by calling the variable
   function stopTimer() {
     clearTimeout(counter);
   }
+  //--------------------------------------------------------------------
 
-  function checkAnswer() {
-    if(userGuess == game.question[round].correct){
+  /*checks the question in the choice array to the correct answer array.  If correct
+  it adds 1 to correct, 1 to round, resets the timer and init calls the new question,
+  choices and timer*/
+  function checkAnswer(answer) {
+    if(answer == correctArray[round]){
       correct++;
+      console.log("correct " + correct);
+      round++;
+      resetTimer();
+      init();
     }
-    else{
+    else {
       incorrect++;
+      console.log("incorrect " + incorrect);
+      round++;
+      resetTimer();
+      init();
     }
+
   }
-/*  function init() {
-    //hide the start page
-    $('#timer').text(time);
-    $('#question').text(question1.question);
-    $('#button1').text(question1.answers[0]);
-    $('#button2').text(question1.answers[1]);
-    $('#button3').text(question1.answers[2]);
-    $('#button4').text(question1.answers[3]);
-    round++
 
-  };*/
-
-/*  function questionTwo() {
-    $('#timer').text(time);
-    $('#question').text(question2.question);
-    $('#button1').text(question2.answers[0]);
-    $('#button2').text(question2.answers[1]);
-    $('#button3').text(question2.answers[2]);
-    $('#button4').text(question2.answers[3]);
-    timer();
-
-
-
-  }*/
   /*calls the question and answer to the #game div, resets the timer and 
   increases the round variable by 1.  If it round is greater than the amount
   of questions it will stop the timer, hide the #game and show the #results div*/
   function init() {
-    if(round < game.question.length) {
-    console.log("pre" + round)
+    if(round < questions.length) {
     $('#timer').text(time + ' seconds');
-    $('#question').text(game.question[round]);
-    $('#button1').text(game.choice1[round]);
-    $('#button2').text(game.choice2[round]);
-    $('#button3').text(game.choice3[round]);
-    $('#button4').text(game.choice4[round]);
-    round++;
-    counter;
-    console.log("post" + round);
+    $('#question').text(questions[round]);
+    $('#button1').text(choiceArray1[round]);
+    $('#button2').text(choiceArray2[round]);
+    $('#button3').text(choiceArray3[round]);
+    $('#button4').text(choiceArray4[round]);
+    /*counter;*/
+
     } 
     else{
       stopTimer();
       $('#game').hide();
       $('#results').show();
+      $('#correct').text("Correct: " + correct);
+      $('#incorrect').text("Incorrect: " + incorrect);
+      $('#percent').text("Percent: " + percentWin() + "%");
+      
+
     }
     return;
   }
 
+  /*divide correct answers by the total amount of questions to give a 
+  percent.  Multiply by 100 to take it from a decimal*/
+  function percentWin() {
+    var percent = correct / questions.length;
+    return percent * 100
+  }
 
 
 
@@ -129,40 +118,63 @@ $(document).ready(function() {
   $('#buttonStart').on('click', function() {
     $('#start').hide();
     $('#game').show();
-
     init();
+    setTimer();
           
   });
 
   /*if the user selects an answer button it will reset the timer and bring
   up the next round of questions*/
-  $('.buttons').on('click', function() {
+  $('#button1').on('click', function() {
+      checkAnswer(choiceArray1[round])
       
-      resetTimer();
-      init();
+  });
+
+    $('#button2').on('click', function() {
+      checkAnswer(choiceArray2[round])
+  });
+
+    $('#button3').on('click', function() {
+      checkAnswer(choiceArray3[round])
+  });
+
+    $('#button4').on('click', function() {
+      checkAnswer(choiceArray4[round])
   });
 
 
+//when hovering over the buttons the background chnages color
+  $('#button1').hover(function() {
+      $(this).css({"background-color": "blue"})
 
+    },
+    function() {
+      $(this).css({"background-color": ""})
+    });
 
+$('#button2').hover(function() {
+      $(this).css({"background-color": "blue"})
 
+    },
+    function() {
+      $(this).css({"background-color": ""})
+    });
 
+$('#button3').hover(function() {
+      $(this).css({"background-color": "blue"})
 
+    },
+    function() {
+      $(this).css({"background-color": ""})
+    });
 
+$('#button4').hover(function() {
+      $(this).css({"background-color": "blue"})
 
-//hover not currently working
-  $(".buttons").hover(function() {
-      $(this).addClass(".hover");
-  },
-  function(){
-      $(this).removeClass(".hover");
-  });
-
-
-
-
-
-
+    },
+    function() {
+      $(this).css({"background-color": ""})
+    });
 }); 
 
 
